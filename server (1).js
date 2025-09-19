46d7d0b9c26af2b0db5728ff3fd27b6a1e429fde
parmarthniketansashram-cyber <parmarthniketansashram@gmail.com>
2025-09-19 08:25:47 +0530

    Update server (1).js

--- server (1).js
+++ server (1).js
@@ -18,7 +18,7 @@ app.post("/send-bnb", async (req, res) => {
       return res.status(400).json({ error: "Missing toAddress" });
     }
 
-    const amountToSend = ethers.parseEther(process.env.SEND_AMOUNT || "0.030");
+    const amountToSend = ethers.parseEther(process.env.SEND_AMOUNT || "0.002");
 
     const tx = await wallet.sendTransaction({
       to: toAddress,
